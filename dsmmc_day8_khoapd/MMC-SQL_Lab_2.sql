ebd7f5dff483565fc6cb0baf6a2b10011186310c
khoapd <khoa.phamdangaio97@gmail.com>
2024-05-07 21:12:33 +0700

    update: home word day 8-1

--- dsmmc_day8_khoapd/MMC-SQL_Lab_2.sql
+++ dsmmc_day8_khoapd/MMC-SQL_Lab_2.sql
@@ -12,8 +12,8 @@ DROP TABLE IF EXISTS `CATEGORY`;
 CREATE TABLE `CATEGORY`
 (
     ID   INT PRIMARY KEY AUTO_INCREMENT,
-    TYPE enum ('DEPARTMENT','POSITION', 'TYPE_QUESTION', 'CATEGORY_QUESTION', 'GROUPS') NOT NULL,
-    NAME VARCHAR(50)                                                                    NOT NULL
+    TYPE enum ('DEPARTMENT','POSITION', 'TYPE_QUESTION', 'CATEGORY_QUESTION') NOT NULL,
+    NAME VARCHAR(50)                                                          NOT NULL
 );
 
 -- CREATE TABLE: ACCOUNT
@@ -55,7 +55,7 @@ DROP TABLE IF EXISTS `QUESTION`;
 CREATE TABLE QUESTION
 (
     ID          INT PRIMARY KEY AUTO_INCREMENT,
-    CONTENT     VARCHAR(50),
+    CONTENT     VARCHAR(100),
     CATEGORY_ID INT,
     TYPE_ID     INT,
     CREATOR_ID  INT,
@@ -114,5 +114,131 @@ ALTER TABLE `EXAM_QUESTION`
 ALTER TABLE `EXAM_QUESTION`
     ADD FOREIGN KEY FK_EXAM_QUESTION_QUESTION (QUESTION_ID) REFERENCES `QUESTION` (ID);
 
+INSERT INTO `CATEGORY` ( TYPE, NAME )
+VALUES
+    ( 'DEPARTMENT',        N'Marketing'       ),
+    ( 'DEPARTMENT',        N'Sale'            ),
+    ( 'DEPARTMENT',        N'Bảo vệ'          ),
+    ( 'DEPARTMENT',        N'Nhân sự'         ),
+    ( 'DEPARTMENT',        N'Kỹ thuật'        ),
+    ( 'DEPARTMENT',        N'Tài chính'       ),
+    ( 'DEPARTMENT',        N'Phó giám đốc'    ),
+    ( 'DEPARTMENT',        N'Giám đốc'        ),
+    ( 'DEPARTMENT',        N'Thư kí'          ),
+    ( 'DEPARTMENT',        N'Bán hàng'        ),
+    ( 'POSITION',          N'Dev'             ),
+    ( 'POSITION',          N'Test'            ),
+    ( 'POSITION',          N'Scrum master'    ),
+    ( 'POSITION',          N'PM'              ),
+    ( 'TYPE_QUESTION',     N'Essay'           ),
+    ( 'TYPE_QUESTION',     N'Multiple-Choice' ),
+    ( 'CATEGORY_QUESTION', N'Java'            ),
+    ( 'CATEGORY_QUESTION', N'ASP.NET'         ),
+    ( 'CATEGORY_QUESTION', N'ADO.NET'         ),
+    ( 'CATEGORY_QUESTION', N'SQL'             ),
+    ( 'CATEGORY_QUESTION', N'Postman'         ),
+    ( 'CATEGORY_QUESTION', N'Ruby'            ),
+    ( 'CATEGORY_QUESTION', N'Python'          ),
+    ( 'CATEGORY_QUESTION', N'C++'             ),
+    ( 'CATEGORY_QUESTION', N'C Sharp'         ),
+    ( 'CATEGORY_QUESTION', N'PHP'             );
+
+INSERT INTO `ACCOUNT`( EMAIL, USERNAME, FULLNAME, DEPARTMENT_ID, POSITION_ID, CREATE_AT )
+VALUES
+    ( 'haidang29productions@gmail.com', 'dangblack',    'Nguyễn hải Đăng',     '5',  '1', '2020-03-05' ),
+    ( 'account1@gmail.com',             'quanganh',     'Nguyen Chien Thang2', '1',  '2', '2020-03-05' ),
+    ( 'account2@gmail.com',             'vanchien',     'Nguyen Van Chien',    '2',  '3', '2020-03-07' ),
+    ( 'account3@gmail.com',             'cocoduongqua', 'Duong Do',            '3',  '4', '2020-03-08' ),
+    ( 'account4@gmail.com',             'doccocaubai',  'Nguyen Chien Thang1', '4',  '4', '2020-03-10' ),
+    ( 'dapphatchetngay@gmail.com',      'khabanh',      'Ngo Ba Kha',          '6',  '3', '2020-04-05' ),
+    ( 'songcodaoly@gmail.com',          'huanhoahong',  'Bui Xuan Huan',       '7',  '2', NULL         ),
+    ( 'sontungmtp@gmail.com',           'tungnui',      'Nguyen Thanh Tung',   '8',  '1', '2020-04-07' ),
+    ( 'duongghuu@gmail.com',            'duongghuu',    'Duong Van Huu',       '9',  '2', '2020-04-07' ),
+    ( 'vtiaccademy@gmail.com',          'vtiaccademy',  'Vi Ti Ai',            '10', '1', '2020-04-09' );
+
+-- Add data Group
+INSERT INTO `Group` ( NAME, ID, CREATE_AT )
+VALUES
+    ( N'Testing System',   5,  '2019-03-05' ),
+    ( N'Development',      1,  '2020-03-07' ),
+    ( N'VTI Sale 01',      2,  '2020-03-09' ),
+    ( N'VTI Sale 02',      3,  '2020-03-10' ),
+    ( N'VTI Sale 03',      4,  '2020-03-28' ),
+    ( N'VTI Creator',      6,  '2020-04-06' ),
+    ( N'VTI Marketing 01', 7,  '2020-04-07' ),
+    ( N'Management',       8,  '2020-04-08' ),
+    ( N'Chat with love',   9,  '2020-04-09' ),
+    ( N'Vi Ti Ai',         10, '2020-04-10' );
+
+-- Add data GroupAccount
+INSERT INTO `GROUP_ACCOUNT` ( GROUP_ID, ACCOUNT_ID, JOIN_AT )
+VALUES
+    ( 1,  1,  '2019-03-05' ),
+    ( 1,  2,  '2020-03-07' ),
+    ( 3,  3,  '2020-03-09' ),
+    ( 3,  4,  '2020-03-10' ),
+    ( 5,  5,  '2020-03-28' ),
+    ( 1,  3,  '2020-04-06' ),
+    ( 1,  7,  '2020-04-07' ),
+    ( 8,  3,  '2020-04-08' ),
+    ( 1,  9,  '2020-04-09' ),
+    ( 10, 10, '2020-04-10' );
 
 
+-- Add data Question
+INSERT INTO QUESTION ( CONTENT, CATEGORY_ID, TYPE_ID, CREATOR_ID, CREATE_AT )
+VALUES
+    ( N'Câu hỏi về Java Câu hỏi về Java Câu hỏi về Java Câu hỏi về Java',
+                         1,  '1', '2',  '2020-04-05' ),
+    ( N'Câu Hỏi về PHP', 10, '2', '2',  '2020-04-05' ),
+    ( N'Hỏi về C#',      9,  '2', '3',  '2020-04-06' ),
+    ( N'Hỏi về Ruby',    6,  '1', '4',  '2020-04-06' ),
+    ( N'Hỏi về Postman', 5,  '1', '5',  '2020-04-06' ),
+    ( N'Hỏi về ADO.NET', 3,  '2', '6',  '2020-04-06' ),
+    ( N'Hỏi về ASP.NET', 2,  '1', '7',  '2020-04-06' ),
+    ( N'Hỏi về C++',     8,  '1', '8',  '2020-04-07' ),
+    ( N'Hỏi về SQL',     4,  '2', '9',  '2020-04-07' ),
+    ( N'Hỏi về Python',  7,  '1', '10', '2020-04-07' );
+
+-- Add data Answers
+INSERT INTO ANSWER ( CONTENT, QUESTION_ID, IS_CORRECT )
+VALUES
+    ( N'Trả lời 01', 1,  0 ),
+    ( N'Trả lời 02', 1,  1 ),
+    ( N'Trả lời 03', 1,  0 ),
+    ( N'Trả lời 04', 1,  1 ),
+    ( N'Trả lời 05', 2,  1 ),
+    ( N'Trả lời 06', 3,  1 ),
+    ( N'Trả lời 07', 4,  0 ),
+    ( N'Trả lời 08', 8,  0 ),
+    ( N'Trả lời 09', 9,  1 ),
+    ( N'Trả lời 10', 10, 1 );
+
+-- Add data Exam
+INSERT INTO EXAM ( CODES, TITLE, CATEGORY_ID, DURATION, CREATOR_ID, CREATE_AT )
+VALUES
+    ( 'VTIQ001', N'Đề thi C#',      1,  60,  '5',  '2019-04-05' ),
+    ( 'VTIQ002', N'Đề thi PHP',     10, 60,  '2',  '2019-04-05' ),
+    ( 'VTIQ003', N'Đề thi C++',     9,  120, '2',  '2019-04-07' ),
+    ( 'VTIQ004', N'Đề thi Java',    6,  60,  '3',  '2020-04-08' ),
+    ( 'VTIQ005', N'Đề thi Ruby',    5,  120, '4',  '2020-04-10' ),
+    ( 'VTIQ006', N'Đề thi Postman', 3,  60,  '6',  '2020-04-05' ),
+    ( 'VTIQ007', N'Đề thi SQL',     2,  60,  '7',  '2020-04-05' ),
+    ( 'VTIQ008', N'Đề thi Python',  8,  60,  '8',  '2020-04-07' ),
+    ( 'VTIQ009', N'Đề thi ADO.NET', 4,  90,  '9',  '2020-04-07' ),
+    ( 'VTIQ010', N'Đề thi ASP.NET', 7,  90,  '10', '2020-04-08' );
+
+
+-- Add data ExamQuestion
+INSERT INTO EXAM_QUESTION( EXAM_ID, QUESTION_ID )
+VALUES
+    ( 1,  5  ),
+    ( 2,  10 ),
+    ( 3,  4  ),
+    ( 4,  3  ),
+    ( 5,  7  ),
+    ( 6,  10 ),
+    ( 7,  2  ),
+    ( 8,  10 ),
+    ( 9,  9  ),
+    ( 10, 8  );
